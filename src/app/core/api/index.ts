1b7719aa075b01e743c790f3b99d020d1ca24e4b
jordipiella <jordipiella@gmail.com>
2021-06-05 09:57:42 +0200

    feat: api module

---
+++ src/app/core/api/index.ts
@@ -0,0 +1,13 @@
+
+// Modules
+export * from './api.module';
+
+
+// Services
+export * from './services/movie/movie.service';
+
+// Contracts
+export * from './services/movie/contracts/movie.contract';
+
+// Enums
+export * from './enums/genre.enum';
