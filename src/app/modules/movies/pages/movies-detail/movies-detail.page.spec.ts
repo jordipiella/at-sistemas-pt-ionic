d254edcf748cb5de76c1fa932ccd4c0f85ff2b91
jordipiella <jordipiella@gmail.com>
2021-06-08 22:20:32 +0200

    test: movies details page and corrections

--- src/app/modules/movies/pages/movies-detail/movies-detail.page.spec.ts
+++ src/app/modules/movies/pages/movies-detail/movies-detail.page.spec.ts
@@ -1,24 +1,158 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { MoviesDetailPage } from './movies-detail.page';
+import { of, throwError } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MoviesFacade } from '../../services/movies.facade';
+import { StoreModule } from '@ngrx/store';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from '../../../../core/header/header.component';
+import { SwitchLanguageComponent } from 'src/app/core/switch-language/switch-language.component';
+import { AppFacade } from '../../../../core/services/app.facade';
+import { movieMockModel } from '../../services/movies/mocks/movie-mock.model';
 
 describe('MoviesDetailPage', () => {
   let component: MoviesDetailPage;
   let fixture: ComponentFixture<MoviesDetailPage>;
+  let appfacade: AppFacade;
+  let moviesFacade: MoviesFacade;
+  let translate: TranslateService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MoviesDetailPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [
+        MoviesDetailPage,
+        HeaderComponent,
+        SwitchLanguageComponent
+      ],
+      imports: [
+        IonicModule.forRoot(),
+        RouterModule.forRoot([]),
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        MoviesFacade,
+        TranslateService,
+        {
+          provide: ActivatedRoute,
+          useValue:  {
+            snapshot: {
+              paramMap: {
+                get: () => '1'
+              }
+            }
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MoviesDetailPage);
+    appfacade = TestBed.inject(AppFacade);
+    moviesFacade = TestBed.inject(MoviesFacade);
+    translate = TestBed.get(TranslateService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#ngOnInit', () => {
+    it('should call loadingSubscription, getMovieId and getMovie', () => {
+      spyOn(component, 'loadingSubscription');
+      spyOn(component, 'getMovieId').and.returnValue(1);
+      spyOn(component, 'getMovie');
+      component.ngOnInit();
+      expect(component.loadingSubscription).toHaveBeenCalled();
+      expect(component.getMovieId).toHaveBeenCalled();
+      expect(component.getMovie).toHaveBeenCalledOnceWith(1);
+    });
+  });
+
+  describe('#ngOnDestroy', () => {
+    it('should call unsubscribe', () => {
+      component.subscriptions = [of().subscribe()];
+      spyOn(component.subscriptions[0], 'unsubscribe');
+
+      component.ngOnDestroy();
+      expect(component.subscriptions[0].unsubscribe).toHaveBeenCalled();
+    });
+  });
+
+  describe('#getMovieId', () => {
+    it('should return id', () => {
+      expect(component.getMovieId()).toEqual(1);
+    });
+  });
+
+  describe('#getMovie', () => {
+    it('should set movie and call movieSelected and loading', () => {
+      component.movie = null;
+      const loadingSpy: jasmine.Spy = spyOnProperty(appfacade, 'loading', 'set');
+      spyOn(moviesFacade, 'getMovie').and.returnValue(of(movieMockModel));
+      const movieSelSpy: jasmine.Spy = spyOnProperty(moviesFacade, 'movieSelected', 'set');
+      component.getMovie(1);
+      expect(component.movie).toEqual(movieMockModel);
+      expect(loadingSpy).toHaveBeenCalledTimes(2);
+      expect(movieSelSpy).toHaveBeenCalled();
+    });
+    it('should call loading and not set movie', () => {
+      component.movie = null;
+      const loadingSpy: jasmine.Spy = spyOnProperty(appfacade, 'loading', 'set');
+      spyOn(moviesFacade, 'getMovie').and.returnValue(throwError({ status: 500}));
+      const movieSelSpy: jasmine.Spy = spyOnProperty(moviesFacade, 'movieSelected', 'set');
+      component.getMovie(1);
+      expect(component.movie).toEqual(null);
+      expect(loadingSpy).toHaveBeenCalledTimes(2);
+      expect(movieSelSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('#loadingSubscription', () => {
+    it('should call setLoading', () => {
+      spyOnProperty(appfacade, 'loading$', 'get').and.returnValue(of(true));
+      spyOn(component, 'setLoading');
+      component.loadingSubscription();
+      expect(component.setLoading).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('#setLoading', () => {
+    it('should set loading', () => {
+      component.setLoading(false);
+      expect(component.loading).toBeFalsy();
+    });
+  });
+
+  describe('#goBack', () => {
+    it('should call navigate', () => {
+      const router: Router = TestBed.inject(Router);
+      spyOn(router, 'navigate');
+      component.goBack();
+      expect(router.navigate).toHaveBeenCalled();
+    });
+  });
+
+  describe('#goToEdit', () => {
+    it('should call navigate', () => {
+      const router: Router = TestBed.inject(Router);
+      spyOn(router, 'navigate');
+      component.goBack();
+      expect(router.navigate).toHaveBeenCalled();
+    });
+  });
+
+  describe('#deleteMovie', () => {
+    it('should call deleteMovie, successToast, translate.instant and GoBack', () => {
+      spyOn(moviesFacade, 'deleteMovie').and.returnValue(of(movieMockModel));
+      spyOn(appfacade, 'successToast');
+      spyOn(translate, 'instant').and.returnValue('success');
+      spyOn(component, 'goBack');
+      component.deleteMovie(1);
+      expect(moviesFacade.deleteMovie).toHaveBeenCalledWith(1);
+      expect(appfacade.successToast).toHaveBeenCalledWith('success');
+      expect(translate.instant).toHaveBeenCalledWith('toast.deletedSuccess');
+      expect(component.goBack).toHaveBeenCalled();
+    });
   });
 });
--- src/app/modules/movies/services/movies/movies.service.spec.ts
+++ src/app/modules/movies/services/movies/movies.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MoviesService } from './movies.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MovieContract, ApiModule, movieMockContract } from '@api';
+import { MovieContract, ApiModule, movieMockContract, GenreEnum } from '@api';
 import { movieMockModel } from './mocks/movie-mock.model';
 import { MovieModel } from './models/movie.model';
 import { ApiMoviesService } from '../../../../core/api/services/api-movies/api-movies.service';
@@ -11,17 +11,35 @@ import { IApiResponse } from '../../../../core/api/interfaces/response.interface
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  const model: MovieModel = {
+    id: 1,
+    title: 'Movie title',
+    poster: 'url/poster.jpg',
+    genre: [ GenreEnum.action ],
+    year: 2001,
+    duration: 130,
+    imdbRating: 9.2,
+    studio: 'Studio',
+    actors: []
+  }
 
-  const movieContrat: MovieContract = movieMockContract;
-
-  const movieModel: MovieModel = movieMockModel;
-
+  const contract: MovieContract = {
+    id: 1,
+    title: 'Movie title',
+    poster: 'url/poster.jpg',
+    genre: [ GenreEnum.action ],
+    year: 2001,
+    duration: 130,
+    imdbRating: 9.2,
+    studio: 'Studio',
+    actors: []
+  }
   const apiMovies: jasmine.SpyObj<ApiMoviesService> = jasmine.createSpyObj('ApiMoviesService', {
-    getAll: of([movieContrat, movieContrat, movieContrat]),
-    get: of(movieContrat),
-    create: of(movieContrat),
-    update: of(movieContrat),
-    delete: of(movieContrat)
+    getAll: of({ total: 5, data: [movieMockContract, movieMockContract, movieMockContract] }),
+    get: of(contract),
+    create: of(contract),
+    update: of(contract),
+    delete: of(movieMockContract)
   });
 
   beforeEach(() => {
@@ -44,7 +62,7 @@ describe('MoviesService', () => {
     it('should call apiMovies.getAll and return a Observable<IApiResponse<MovieModel[]>>', () => {
       service.getAll()
         .subscribe((res: IApiResponse<MovieModel[]>) => {
-          expect(res.data).toEqual([movieModel, movieModel, movieModel]);
+          expect(res.data).toEqual([movieMockContract, movieMockContract, movieMockContract]);
         });
       expect(apiMovies.getAll).toHaveBeenCalled();
     });
@@ -52,42 +70,43 @@ describe('MoviesService', () => {
 
   describe('#get()', () => {
     it('should call apiMovies.get and return a Observable<MovieModel> by Id', () => {
-      service.get(movieModel.id)
+      service.get(model.id)
         .subscribe((movie: MovieModel) => {
-          expect(movie).toEqual(movieModel);
+          expect(movie).toEqual(model);
         });
-        expect(apiMovies.get).toHaveBeenCalledWith(movieModel.id);
+        expect(apiMovies.get).toHaveBeenCalledWith(model.id);
     });
   });
 
   describe('#create()', () => {
     it('should call apiMovies.create and return a Observable<MovieModel>', () => {
-      service.create(movieModel)
+      service.create(model)
         .subscribe((movie: MovieModel) => {
-          expect(movie).toEqual(movieModel);
+          expect(movie).toEqual(model);
         });
-        expect(apiMovies.create).toHaveBeenCalledWith(movieModel);
+        expect(apiMovies.create).toHaveBeenCalledWith(model);
       });
   });
 
   describe('#update()', () => {
     it('should call apiMovies.update url and return a new movie as Observable<MovieModel>', () => {
-      service.update(movieModel)
+
+      service.update(model)
         .subscribe((movie: MovieModel) => {
-          expect(movie).toEqual(movieModel);
+          expect(movie).toEqual(model);
         });
-        expect(apiMovies.update).toHaveBeenCalledWith(movieModel);
+        expect(apiMovies.update).toHaveBeenCalledWith(model);
       });
   });
 
   describe('#delete()', () => {
     it('should call apiMovies.delete url and return a Observable<MovieModel>', () => {
-      service.delete(movieModel.id).subscribe(
-        (movie: MovieModel) => {
-          expect(movie).toEqual(movieModel);
+      service.delete(movieMockModel.id).subscribe(
+        (movie: MovieContract) => {
+          expect(movie).toEqual(movieMockContract);
         }
       );
-        expect(apiMovies.delete).toHaveBeenCalledWith(movieModel.id);
+      expect(apiMovies.delete).toHaveBeenCalledWith(movieMockModel.id);
       });
   });
 
